3ab1015ec8552dd7bd9688b0a31f3e81804a9a6b
Isha Wilson <isharainbowlight@gmail.com>
2018-03-28 12:50:00 -0700

    changed way too many things for a commit after not paying attention too long. New pages, new design, new images, etc. Things roughly functional now luckily.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SubpageComponent} from './subpage/subpage.component';
+import {ShippingComponent} from './shipping/shipping.component';
+import {SundanceWineCellarsComponent} from './sundance-wine-cellars/sundance-wine-cellars.component';
 import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'subpage', component: SubpageComponent },
+  { path: 'shipping', component: ShippingComponent },
+  { path: 'sundance_wine_cellars', component: SundanceWineCellarsComponent },
+
 ];
 
 @NgModule({
