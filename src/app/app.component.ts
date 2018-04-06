89fb58bf469110bf02d410bf05fcfc7113177d12
Isha Wilson <isharainbowlight@gmail.com>
2018-04-05 19:50:26 -0700

    Fixed responsive navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
+// @module({
+//   declarations: [AppComponent,],
+//   imports: [NgbModule,]
+// })
+
 export class AppComponent {
+  public isNavbarCollapsed = true;
 }
