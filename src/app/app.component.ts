9cd41c26500c188c94295a15fe7b7bfef36d4c0b
Isha Wilson <isharainbowlight@gmail.com>
2023-02-26 20:47:44 -0800

    Age verification modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,6 +41,7 @@ export class AppComponent {
         this.routeData = defaultRouteData;
         this.updateRouteDataFrom(router.routerState.snapshot.root);
         this.handleUpdatedRouteData();
+        this.handleAgeModal();
       }
     });
   }
@@ -59,5 +60,51 @@ export class AppComponent {
   private handleUpdatedRouteData() {
     this.title.setTitle(this.routeData.title);
   }
+
+  private handleAgeModal() {
+    // Get the modal elements
+    var modal = document.getElementById("age-verify-modal");
+    var yesBtn = document.getElementById("age-verify-yes");
+    var noBtn = document.getElementById("age-verify-no");
+
+    // If the user hasn't verified age recently, open the modal
+    if (!this.readAgeCookie('age_verify')) {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks yes, close the modal and create a cookie
+    yesBtn.onclick = () => {
+      this.createAgeCookie('age_verify', 'true', 1);
+      modal.style.display = "none";
+    }
+
+    // When the user clicks no, redirect to Sundance grocery site
+    noBtn.onclick = function() {
+      window.location.assign('https://sundancenaturalfoods.com/')
+    }
+  }
+
+  private createAgeCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (1 * 24 * 60 * 60 * 1000));
+      console.log(date);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/"; // affects all site pages
+  }
+
+  private readAgeCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
 }
 
